9cabb5144239ee3dd9618bab642bf66e2b62d53f
dokyun.kim <mongte32@gmail.com>
2022-07-20 10:56:00 +0900

    feat: git workflow test3

---
+++ src/app/components/dddddd.module.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'name',
+  templateUrl: 'name.component.html'
+})
+
+export class NameComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
